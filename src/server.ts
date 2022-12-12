6e3ba99632b1fff669e6cdd5b3edea87fb70a012
saagas.code <saagas.code@gmail.com>
2022-12-12 18:53:54 -0300

    fix deploy

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 
 import { app } from './app';
 
-app.listen(process.env.PORT, async () => {
+app.listen(process.env.PORT || 8819, async () => {
   console.log(`Aplicativo rodando na porta: ${process.env.PORT}!`)
 })
